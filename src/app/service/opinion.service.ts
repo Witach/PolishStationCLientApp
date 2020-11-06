b13c384ffc6c21498b2644f6da92c9b2fc2fb63e
witaszek98@wp.pl <witaszek98@wp.pl>
2020-11-06 18:23:14 +0100

    opinion service implemented

---
+++ src/app/service/opinion.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import {Observable} from "rxjs";
+import {OpinionDto, OpinionPostDto} from "../../api-models/api-models";
+import {HttpClient} from "@angular/common/http";
+import {environment} from "../../environments/environment";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OpinionService {
+
+  constructor(private http: HttpClient) { }
+
+  sendOpinion(opinionPostDTO: OpinionPostDto): Observable<OpinionDto> {
+    return this.http.post<OpinionDto>(environment.apiUrl + '/opinion', opinionPostDTO);
+  }
+}
