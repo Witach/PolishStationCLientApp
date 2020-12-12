e3f545f1102d07328dd797089da38487fcb6c464
witaszek98@wp.pl <witaszek98@wp.pl>
2020-12-12 16:44:34 +0100

    virtualization bug fixed

--- src/app/main-app/petrol-station-details/petrol-station-details.component.ts
+++ src/app/main-app/petrol-station-details/petrol-station-details.component.ts
@@ -245,7 +245,9 @@ export class PetrolStationDetailsComponent implements OnInit, AfterViewInit {
 
   stringifyDateHumanReadable(date: string) {
     const dateObj = new Date(date);
-    return dateObj.getDay() + '.' + (dateObj.getMonth() + 1) + '.' + dateObj.getFullYear() + 'r';
+    const dayText = dateObj.getDate() < 10 ? '0' + dateObj.getDate() : dateObj.getDate();
+    const monthText = (dateObj.getMonth() + 1) < 10 ? '0' + (dateObj.getMonth() + 1) : (dateObj.getMonth() + 1);
+    return dayText + '.' + monthText + '.' + dateObj.getFullYear() + 'r';
   }
 
   showPreviligeBar() {
